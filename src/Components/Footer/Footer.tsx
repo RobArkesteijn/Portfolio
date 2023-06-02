58c00f74215b21ebc73c59be9a70b0dfaaf0cb96
RobArkesteijn <rarkesteijn@outlook.com>
2023-06-02 14:36:21 +0200

    Added rel=noreferrer to prevent eslint error

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,9 +5,9 @@ function Footer() {
   return (
     <footer className='footer'>
       <ul className='footer-list'>
-        <li><a className='footer-list-item' target='_blank' href="mailto:rarkesteijn@outlook.com"><i className="fa-solid fa-envelope"></i></a></li>
-        <li><a className='footer-list-item' target='_blank' href="https://www.linkedin.com/in/rob-arkesteijn-394550267/"><i className="fa-brands fa-linkedin"></i></a></li>
-        <li><a className='footer-list-item' target='_blank' href="https://github.com/RobArkesteijn"><i className="fa-brands fa-github"></i></a></li>
+        <li><a className='footer-list-item' target='_blank' rel="noreferrer" href="mailto:rarkesteijn@outlook.com"><i className="fa-solid fa-envelope"></i></a></li>
+        <li><a className='footer-list-item' target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/rob-arkesteijn-394550267/"><i className="fa-brands fa-linkedin"></i></a></li>
+        <li><a className='footer-list-item' target='_blank' rel="noreferrer" href="https://github.com/RobArkesteijn"><i className="fa-brands fa-github"></i></a></li>
       </ul>
     </footer>
   )
